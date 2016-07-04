7bc830bf16158b42680b08cbcff183e1b10a27d7
fanlifeng <362517415@qq.com>
2016-07-04 15:18:54 +0800

    新创建的新项目

---
+++ PaiQuan/PaiQuan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaiQuan
+//
+//  Created by Mac on 16/7/4.
+//  Copyright © 2016年 huigu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaiQuan/PaiQuan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaiQuan
+//
+//  Created by Mac on 16/7/4.
+//  Copyright © 2016年 huigu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
